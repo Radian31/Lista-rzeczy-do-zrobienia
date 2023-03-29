c5c3194b065ed40100288a92ee5e3fb9ed435847
Adrian Sikorski <radian3113@gmail.com>
2023-03-29 23:36:00 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+{
+    const tasks = [
+        {
+            content: "Wyjść do pracy",
+            done: false,
+        },
+        {
+            content: "zjeść obiad",
+            done: true,
+        },
+    ];
+    const render = () => {
+        let htmlString = "";
+        for (const task of tasks) {
+            htmlString += `
+           <li>
+           ${task.content}
+           </li>
+            `;
+        }
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    }
+
+    const init = () => {
+        render(); 
+    };
+    init();
+}   
