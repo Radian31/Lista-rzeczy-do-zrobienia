9997b70d9e4cd140a8d42f1b38abc5c75d4c3721
Adrian Sikorski <radian3113@gmail.com>
2023-04-02 19:09:51 +0200

    adding new Task and showing if a task is done

--- js/script.js
+++ js/script.js
@@ -2,27 +2,53 @@
     const tasks = [
         {
             content: "Wyjść do pracy",
-            done: false,
+            done: true,
         },
         {
             content: "zjeść obiad",
-            done: true,
+            done: false,
         },
     ];
     const render = () => {
         let htmlString = "";
         for (const task of tasks) {
             htmlString += `
-           <li>
-           ${task.content}
+            <li
+            ${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+            ${task.content}
            </li>
             `;
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
     }
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+
+        });
+        render();
+    }
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        if (newTaskContent === "") {
+            return;
+
+        };
+        addNewTask(newTaskContent);
+    };
+
+
     const init = () => {
-        render(); 
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
     init();
-}   
+}
+
+
